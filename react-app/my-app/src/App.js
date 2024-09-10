c772532290642a6c2ad5ac588043bbc735805527
Usmang843 <usman1urrehman2@gmail.com>
2024-09-10 16:36:52 +0500

    updation

--- react-app/my-app/src/App.js
+++ react-app/my-app/src/App.js
@@ -1,26 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/myHeader"; // importing is the file function by which is exporting by file doesn't matter what name file is exporting
 function App() {
   return (
     <>
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Simple React app start with: npm start
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-    <div></div>
+      <div className="App">
+        <Header />
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Simple React app start with: npm start</p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
     </>
   );
 }
---
+++ vite-project/src/component/Header.jsx
@@ -0,0 +1,6 @@
+const Header = () =>{
+    return <div>Usman Ur Rehman! -- Hello React With Vite </div>
+    // {import.meta.env.VITE_API_URL} place in the div and is the global variable we use in react-vite app
+}
+
+export default Header
