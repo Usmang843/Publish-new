70b7afb923022b54b7c3ead4e65e44089fd7d1c5
Usman Ur Rehman <usman1urrehman2@gmail.com>
2024-09-17 14:00:20 +0500

    updation

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -4,12 +4,14 @@
 // import Header from "./component/Header";
 import Comments from "./component/Comments";
 import Mypage from "./component/Mypage";
+import ReactL from "./component/ReactL"
 import "./App.css"
 
 function App() {
   return (
     <div>
       {/* <Header /> */}
+      <ReactL />
       <Comments />
       <Mypage/>
     </div>
---
+++ vite-project/src/component/ReactL.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ReactL = () => {
+  return (
+    <>
+    {/* first install node
+        npm create vite@latest -> npm install -> npm run dev
+    */}
+    </>
+  );
+}
+export default ReactL;
