441b414f6f4c7e181fbef3e0f90dd5694aa36d94
Usman Ur Rehman <usman1urrehman2@gmail.com>
2024-09-17 18:38:20 +0500

    updation

--- vite-project/src/component/ReactL.jsx
+++ vite-project/src/component/ReactL.jsx
@@ -5,7 +5,10 @@ const ReactL = () => {
     <>
     {/* first install node
         npm create vite@latest -> npm install -> npm run dev
-        
+        react uses bundlers 
+        Hooks -> react updates and control UI 
+        Diffing of lists is performed using keys, keys should be stable, predictable and unique
+                
     */}
     </>
   );
