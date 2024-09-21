9eeaf96ff2d401c11a5f7b40c5dbadff882beacb
Usman Ur Rehman <usman1urrehman2@gmail.com>
2024-09-21 15:44:01 +0500

    updation

---
+++ MegaProject/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <h1>Hello This is our Mega Application which using full-Stack developement</h1>
+    </>
+  )
+}
+
+export default App
